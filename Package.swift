2258701f6476344cd83ff3a0147c6459185b30f7
Sergey Berg <kotelberg@gmail.com>
2026-06-13 17:07:47 +0400

    MapLibre 1.0.0 (HataHub fork, R2-hosted xcframework)

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+// HataHub fork of MapLibre Native (Metal + GL), built from
+// github.com/Kotelberg/maplibre-native. The xcframework binary is hosted on R2;
+// this package is just the manifest so EAS / SPM can fetch it remotely.
+let package = Package(
+    name: "MapLibre",
+    products: [
+        .library(name: "MapLibre", targets: ["MapLibre"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MapLibre",
+            url: "https://map.hatahub.com.ua/spm/MapLibre-1.0.0.xcframework.zip",
+            checksum: "f0ab3ce7a05a7603e3365f97239494b5be6c5b86128c1cb6b876252f462097ab"
+        )
+    ]
+)
